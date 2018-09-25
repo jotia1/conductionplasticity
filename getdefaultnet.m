3c1522fb82b7c0433bdc36ba192b9073e7da2072
Joshua Arnold <j.arnold4@uq.edu.au>
2018-09-25 17:03:09 +1000

    found sdvl issue

--- getdefaultnet.m
+++ getdefaultnet.m
@@ -13,11 +13,11 @@ net.rand_seed = 1;
 net.delays = zeros(net.N);
 net.delays(1:3, 4:5) = 1;
 net.variance = zeros(net.N);
-net.variance(1:3, 4:5) = 0.1;
+net.variance(1:3, 4:5) = 1;
 net.w = zeros(net.N);
 net.w(1:3, 4:5) = 1;
 
-net.fgi = 130;
+net.fgi = 70;
 net.nu = 0.03;
 net.nv = 0.01;
 net.a1 = 3;
@@ -35,13 +35,22 @@ net.v_thres = -55;
 net.w_max = 10;
 net.taupre = 20;
 net.taupost = 20;
-net.Apost = 0.1;
-net.Apre = -0.1;
+net.Apost = 0;%0.1;
+net.Apre = 0;%-0.1;
 
-net.sim_time_sec = 1;
-net.inp = [1, 2, 3, 1, 2 ,3];
-net.ts = [1, 5, 8, 501, 505, 508];
+net.sim_time_sec = 100;
+seq = [0, 3, 7];
+net.inp = repmat(1:3, 1, net.sim_time_sec * 2); %[1, 2, 3, 1, 2 ,3];
+net.ts = reshape(repmat(((0:(net.sim_time_sec * 2) -1 )' * 500)', 3, 1), 1, net.sim_time_sec * 3 * 2) + ...
+        repmat(seq, 1, net.sim_time_sec * 2) + 1; %[1, 5, 8, 501, 505, 508];
 
+% Set up supervision
+net.supervised_seconds = 70;
+net.inp = [net.inp, ones(1, net.supervised_seconds * 2) * length(seq) + 1];
+net.ts = [net.ts, ((0:0.5:net.supervised_seconds - 0.5) * 1000) + 13];
+ 
+    
+    
 net.voltages_to_save = [1 : net.N];
 
 
--- spikingnet.m
+++ spikingnet.m
@@ -30,8 +30,19 @@ STDPdecaypost = exp(-1/net.taupost);
 active_spikes = cell(net.delay_max, 1);  % To track when spikes arrive
 active_idx = 1;
 
-for sec = 1 :net.sim_time_sec
-    
+% output variables
+outp.timing_info.init_time = toc(outp.timing_info.init_time);
+outp.timing_info.sim_sec_tics = uint64(zeros(net.sim_time_sec, 1));
+outp.timing_info.sim_sec_tocs = zeros(net.sim_time_sec, 1);
+outp.timing_info.full_sec_tocs = zeros(net.sim_time_sec, 1);
+outp.timing_info.plotting_tics = uint64([]);
+outp.timing_info.plotting_tocs = [];
+
+outp.spike_time_trace = [];
+debug = zeros(net.sim_time_sec * ms_per_sec, 6);
+
+for sec = 1 : net.sim_time_sec
+    outp.timing_info.sim_sec_times(sec) = tic;
     spike_time_trace = [];
     
     % Trim data into seconds to speed searching later
@@ -42,7 +53,6 @@ for sec = 1 :net.sim_time_sec
     for ms = 1 : ms_per_sec
         
         time = (sec - 1) * ms_per_sec + ms;
-        vt(:, time) = v(net.voltages_to_save);
         
         %% Calculate input
         Iapp = zeros(size(v));
@@ -53,6 +63,9 @@ for sec = 1 :net.sim_time_sec
         gaussian_values = w .* g;
         gaussian_values(isnan(gaussian_values)) = 0;
         Iapp = sum(gaussian_values, 1)';
+
+        debug(time, 1:3) = delays(1:3, 4)';
+        debug(time, 4:6) = variance(1:3, 4)';
         
         %% Update membrane voltages  
         v = v + (net.v_rest + Iapp - v) / net.neuron_tau;
@@ -61,7 +74,12 @@ for sec = 1 :net.sim_time_sec
         %% Deal with neurons that just spiked
         fired_naturally = find(v >= net.v_thres);
         fired_pixels = inp_trimmed(ts_trimmed == time);
-        fired = unique([fired_naturally; fired_pixels';]); % unique in case of SDVL supervision later
+        %  if      supervised                and       it fired recently         or     is firing now.
+        if numel(find(fired_pixels == 4)) > 0 && (time - last_spike_time(4) < 30 || numel(find(fired_naturally == 4)) > 0)   % Only supervise if we havent seen a pixel 4 fire recently.
+            fired_pixels(find(fired_pixels == 4)) = [];
+            fprintf('Remove supervision %d\n', sec*1000 + ms);
+        end
+        fired = [fired_naturally; fired_pixels';]; % unique in case of SDVL supervision later
         spike_time_trace = [spike_time_trace; time*ones(length(fired),1), fired];
         last_spike_time(fired) = time; 
         
@@ -70,7 +88,7 @@ for sec = 1 :net.sim_time_sec
         %% STDP
         % Any pre-synaptics weights should be increased
         w(:, fired) = w(:, fired) + (dApost(:, fired) .* conns(:, fired));
-        % any post synaptic weights decreased
+        % Any post synaptic weights decrease
         w(fired, :) = w(fired, :) + (dApre(fired, :) .* conns(fired, :));
         dApost(fired, :) = dApost(fired, :) + net.Apost;
         dApre(:, fired) = dApre(:, fired) + net.Apre;
@@ -89,14 +107,14 @@ for sec = 1 :net.sim_time_sec
         du = zeros(size(t0_negu));
         du(t0 > net.a2) = -k(t0 > net.a2) .* net.nu;
         du(abst0_negu >= net.a1) = shifts(abst0_negu >= net.a1);% TODO: verify this line is correct, made an edit without checkign the maths.
- 
+
         delays(:, fired) = delays(:, fired) + (du .* conns(:, fired));
         delays(conns) = max(1, min(net.delay_max, delays(conns)));
 
         % Update SDVL variance
         dv = zeros(size(t0_negu));
-        dv(abst0_negu < net.b2) = -k(abst0_negu < net.b2);
-        dv(abst0_negu >= net.b1) = k(abst0_negu >= net.b1);
+        dv(abst0_negu < net.b2) = -k(abst0_negu < net.b2) .* net.nv;
+        dv(abst0_negu >= net.b1) = k(abst0_negu >= net.b1) .* net.nv;
 
         variance(:, fired) = variance(:, fired) + (dv .* conns(:, fired));
         variance(conns) = max(net.variance_min, min(net.variance_max, variance(conns)));
@@ -109,14 +127,28 @@ for sec = 1 :net.sim_time_sec
         % Synaptic bounding - limit w to [0, w_max]
         w = max(0, min(net.w_max, w));        
         
-    end
+    end  % of ms for loop
 
     
-end
+outp.spike_time_trace = [outp.spike_time_trace; spike_time_trace]; % TODO - optimise for speed if necessary
+outp.timing_info.full_sec_tocs(sec) = toc(outp.timing_info.sim_sec_times(sec));
+fprintf('Second: %d, Elapsed: %.3f \n', sec, outp.timing_info.full_sec_tocs(sec));
 
-plot(vt(4:5, :)')
+end    % of seconds for loop
+
+%plot(vt(4, :)')
+subplot(2, 1, 1)
+plot(debug(:, 1:3));
+title('delay');
+subplot(2, 1, 2)
+plot(debug(:, 4:6));
 %set(gca, 'Color', 'k')
-legend({'N4', 'N5'});
+legend({'N4'});
+
+outp.vt = vt;
+outp.w = w;
+outp.variance = variance;
+outp.delays = delays;
 
 end
 
